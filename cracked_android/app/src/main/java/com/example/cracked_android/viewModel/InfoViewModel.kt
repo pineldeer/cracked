a41d9ecf100620eeabf2190aa0ec08bb2d386a03
snupinel <63478081+pineldeer@users.noreply.github.com>
2025-05-11 00:51:50 +0900

    infoviewmodel 변수들 작성

--- cracked_android/app/src/main/java/com/example/cracked_android/viewModel/InfoViewModel.kt
+++ cracked_android/app/src/main/java/com/example/cracked_android/viewModel/InfoViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.cracked_android.viewModel
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import com.example.cracked_android.data.PrefRepository
 import com.example.cracked_android.network.MyRestAPI
+import com.example.cracked_android.network.dto.UserInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +16,36 @@ class InfoViewModel @Inject constructor(
     private val api: MyRestAPI,
     private val prefRepository: PrefRepository,
 ) :ViewModel(){
+
+    private val _imageUri:MutableStateFlow<Uri?> = MutableStateFlow(null)
+    val imageUri: StateFlow<Uri?> = _imageUri.asStateFlow()
+
+    fun setImageUri(value:Uri){
+        _imageUri.value = value
+    }
+
+    private val _username = MutableStateFlow("")
+    val username: StateFlow<String> = _username.asStateFlow()
+
+    fun setUsername(value:String){
+        _username.value = value
+    }
+
+    private val _genderIsMale:MutableStateFlow<Boolean?> = MutableStateFlow(null)
+    val genderIsMale: StateFlow<Boolean?> = _genderIsMale.asStateFlow()
+
+    fun setGender(isMale:Boolean){
+        _genderIsMale.value = isMale
+    }
+
+    private val _age = MutableStateFlow(20)
+    val age: StateFlow<Int> = _age.asStateFlow()
+
+    fun setAge(value:Int){
+        _age.value = value
+    }
+
+
+
 
 }
