e7035464789e2e6e177fb80465839fc837372e8f
snupinel <63478081+pineldeer@users.noreply.github.com>
2025-05-10 22:58:05 +0900

    MyApplication 진입점 설정

--- cracked_android/app/src/main/java/com/example/cracked_android/MainActivity.kt
+++ cracked_android/app/src/main/java/com/example/cracked_android/MainActivity.kt
@@ -10,19 +10,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cracked_android.pages.StartPage
 import com.example.cracked_android.ui.theme.Cracked_androidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Cracked_androidTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                if(true){
+                    MyApp(startDestination = "MainPage")
+                }else{
+                    MyApp(startDestination = "MainPage")
                 }
             }
         }
@@ -30,17 +36,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun MyApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "StartPage"
+) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Cracked_androidTheme {
-        Greeting("Android")
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        NavHost(navController = navController, startDestination = startDestination) {
+            composable("StartPage") {
+                StartPage(navController)
+            }
+
+
+        }
     }
+
 }
---
+++ cracked_android/app/src/main/java/com/example/cracked_android/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.cracked_android
+
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class MyApplication {
+}
