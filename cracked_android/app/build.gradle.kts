175b5261e084cdd0780263b4afde6f28e8e8f9de
snupinel <63478081+pineldeer@users.noreply.github.com>
2025-05-10 22:49:32 +0900

    필요 라이브러리 추가
    coli, daggerhilt, navigation, okhttp, retrofit, moshi

--- cracked_android/app/build.gradle.kts
+++ cracked_android/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -50,6 +52,26 @@ android {
 }
 
 dependencies {
+
+    //Coli
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
+    //Dagger-Hilt
+    implementation("com.google.dagger:hilt-android:2.49")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.7")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    // moshi
+    implementation("com.squareup.moshi:moshi:1.14.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
