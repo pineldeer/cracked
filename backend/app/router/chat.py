12349c15aa6762d5529d03bd31e4f4dee7016774
mjsky514 <minjoon0514@gmail.com>
2025-05-10 22:09:54 +0900

    OpenAI API를 사용하여 질문 생성 및 답변 기능 추가, 환경 변수 로드 기능 구현

--- backend/app/router/chat.py
+++ backend/app/router/chat.py
@@ -1,26 +1,79 @@
 from fastapi import APIRouter, HTTPException, UploadFile, File
+from app.config import OPENAI_API_KEY
+from openai import OpenAI
 
+from database import create_connection
 
 router = APIRouter()
 
-# ai 질문 생성
-@router.post("/create_question/{user_id}")
-def create_question(user_id: str):
-    # DB에 user_id 가지고 DB user 테이블에 유저 정보 조회
-    return {"message": "User info retrieved successfully"}
-
-# ai 질문 답변
-@router.post("/answer_question/{user_id}")
-def answer_question(user_id: str, chat_order: int):
-    # DB에 user_id 가지고 DB user 테이블에 유저 정보 조회
-    return {"message": "User info retrieved successfully"}
 
 # 모든 질문 get
 @router.get("/get_all_chat/{user_id}")
 def get_all_chat(user_id: str):
     # DB에 user_id 가지고 DB user 테이블에 유저 정보 조회
-    return {"message": "User info retrieved successfully"}
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM chat WHERE user_id = ?", (user_id,))
+    chats = cursor.fetchall()
+    conn.close()
+    return [
+        {
+        "id": chat[0],
+        "user_id": chat[1],
+        "question": chat[2],
+        "answer": chat[3],
+        "order_idx": chat[4],
+        "created_at": chat[5]
+        }
+        for chat in chats
+    ]
 
+# ai 질문 생성
+@router.post("/create_question/{user_id}")
+def create_question(user_id: str):
 
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
+    messages = [{"role": "system", "content": "You are a helpful assistant that create question for user."}]
+    previous_chats = get_all_chat(user_id)
+    for chat in previous_chats:
+        messages.append({"role": "assistant", "content": chat["question"]})
+        messages.append({"role": "user", "content": chat["answer"]})
 
+    response = client.chat.completions.create(
+            model="gpt-4o-mini",  # or "gpt-3.5-turbo"
+            messages=messages
+        )
+    
+    previous_chats_len = len(previous_chats)
+    
+    question = response.choices[0].message.content.strip()
+
+    # 질문 저장
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO chat (user_id, question, answer, order_idx) VALUES (?, ?, ?, ?)", (user_id, question, "", previous_chats_len))
+    conn.commit()
+    # 질문 저장 후 질문 번호 반환
+    cursor.execute("SELECT * FROM chat WHERE user_id = ? AND order_idx = ?", (user_id, previous_chats_len))
+    chat = cursor.fetchone()
+    conn.close()
+    return {
+        "id": chat[0],
+        "user_id": chat[1],
+        "question": chat[2],
+        "answer": chat[3],
+        "order_idx": chat[4],
+        "created_at": chat[5]
+    }
+
+# ai 질문 답변
+@router.post("/answer_question/{user_id}")
+def answer_question(user_id: str, order_idx: int, answer: str):
+    # DB에 user_id 가지고 DB user 테이블에 유저 정보 조회
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE chat SET answer = ? WHERE user_id = ? AND order_idx = ?", (answer, user_id, order_idx))
+    conn.commit()
+    conn.close()
+    return {"message": "Answer updated successfully"}
